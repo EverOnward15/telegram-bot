e9f8bdd5aa2c697845966dfb910c3e4b2b7e0de3
EverOnward15 <leiste15@gmail.com>
2026-04-05 06:57:09 +0530

    Updated port

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { Telegraf } = require("telegraf");
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Telegram bot setup
 const bot = new Telegraf("8043063927:AAHtd0_EqjuR_mjRk_VriFHfOpu_PqWcNk8");
 
 bot.start((ctx) => {
@@ -9,3 +14,13 @@ bot.start((ctx) => {
 bot.launch();
 
 console.log("Bot is running...");
+
+// Dummy route (optional but nice to have)
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+// Start server (this is what Render needs)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
